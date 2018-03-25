8bc8a93d4185623afba059ee84f070f9bffd9fdd
Frander Granados <fgranados@ic-itcr.ac.cr>
2018-03-25 02:29:20 -0600

    Delete backup_cloud.sh

--- backup_cloud.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#script para respaldo de datos en owncloud y/o nextcloud
-#Realizado por Frander Granados V.
-#email fgranados@ic-itcr.ac.cr
-#git https://github.com/Franderg
-#page frander.xyz
-
-
